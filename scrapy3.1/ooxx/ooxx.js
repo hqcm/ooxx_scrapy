f71a74f89f372e334ccccfa27256e89cd7d6a4e7
hqcm <1973132883@qq.com>
2018-07-03 22:37:53 +0800

    initial commit

--- scrapy3.1/ooxx/ooxx.js
+++ scrapy3.1/ooxx/ooxx.js
@@ -254,3 +254,176 @@ function binl2b64(binarray)
   }
   return str;
 }
+function Base64() {
+ 
+  // private property
+  _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+  // public method for encoding
+  this.encode = function (input) {
+      var output = "";
+      var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+      var i = 0;
+      input = _utf8_encode(input);
+      while (i < input.length) {
+          chr1 = input.charCodeAt(i++);
+          chr2 = input.charCodeAt(i++);
+          chr3 = input.charCodeAt(i++);
+          enc1 = chr1 >> 2;
+          enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+          enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+          enc4 = chr3 & 63;
+          if (isNaN(chr2)) {
+              enc3 = enc4 = 64;
+          } else if (isNaN(chr3)) {
+              enc4 = 64;
+          }
+          output = output +
+          _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+          _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+      }
+      return output;
+  }
+
+  // public method for decoding
+  this.decode = function (input) {
+      var output = "";
+      var chr1, chr2, chr3;
+      var enc1, enc2, enc3, enc4;
+      var i = 0;
+      input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+      while (i < input.length) {
+          enc1 = _keyStr.indexOf(input.charAt(i++));
+          enc2 = _keyStr.indexOf(input.charAt(i++));
+          enc3 = _keyStr.indexOf(input.charAt(i++));
+          enc4 = _keyStr.indexOf(input.charAt(i++));
+          chr1 = (enc1 << 2) | (enc2 >> 4);
+          chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+          chr3 = ((enc3 & 3) << 6) | enc4;
+          output = output + String.fromCharCode(chr1);
+          if (enc3 != 64) {
+              output = output + String.fromCharCode(chr2);
+          }
+          if (enc4 != 64) {
+              output = output + String.fromCharCode(chr3);
+          }
+      }
+      output = _utf8_decode(output);
+      return output;
+  }
+
+  // private method for UTF-8 encoding
+  _utf8_encode = function (string) {
+      string = string.replace(/\r\n/g,"\n");
+      var utftext = "";
+      for (var n = 0; n < string.length; n++) {
+          var c = string.charCodeAt(n);
+          if (c < 128) {
+              utftext += String.fromCharCode(c);
+          } else if((c > 127) && (c < 2048)) {
+              utftext += String.fromCharCode((c >> 6) | 192);
+              utftext += String.fromCharCode((c & 63) | 128);
+          } else {
+              utftext += String.fromCharCode((c >> 12) | 224);
+              utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+              utftext += String.fromCharCode((c & 63) | 128);
+          }
+
+      }
+      return utftext;
+  }
+
+  // private method for UTF-8 decoding
+  _utf8_decode = function (utftext) {
+      var string = "";
+      var i = 0;
+      var c = c1 = c2 = 0;
+      while ( i < utftext.length ) {
+          c = utftext.charCodeAt(i);
+          if (c < 128) {
+              string += String.fromCharCode(c);
+              i++;
+          } else if((c > 191) && (c < 224)) {
+              c2 = utftext.charCodeAt(i+1);
+              string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+              i += 2;
+          } else {
+              c2 = utftext.charCodeAt(i+1);
+              c3 = utftext.charCodeAt(i+2);
+              string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+              i += 3;
+          }
+      }
+      return string;
+  }
+}
+function time() {
+    var a = new Date().getTime();
+    return parseInt(a / 1000)
+}
+function chr(a) {
+    return String.fromCharCode(a)
+}
+function ord(a) {
+    return a.charCodeAt()
+}
+function md5(a) {
+    return hex_md5(a)
+}
+var ooxx = function(n, t, e) {
+    var f = "DECODE";
+    var t = t ? t: "";
+    var e = e ? e: 0;
+    var r = 4;
+    t = md5(t);
+    var d = n;
+    var p = md5(t.substr(0, 16));
+    var o = md5(t.substr(16, 16));
+    var base64=new Base64
+    if (r) {
+        if (f == "DECODE") {
+            var m = n.substr(0, r)
+        }
+    } else {
+        var m = ""
+    }
+    var c = p + md5(p + m);
+    var l;
+    if (f == "DECODE") {
+        n = n.substr(r);
+        l = base64.decode(n)
+    }
+    var k = new Array(256);
+    for (var h = 0; h < 256; h++) {
+        k[h] = h
+    }
+    var b = new Array();
+    for (var h = 0; h < 256; h++) {
+        b[h] = c.charCodeAt(h % c.length)
+    }
+    for (var g = h = 0; h < 256; h++) {
+        g = (g + k[h] + b[h]) % 256;
+        tmp = k[h];
+        k[h] = k[g];
+        k[g] = tmp
+    }
+    var u = "";
+    l = l.split("");
+    for (var q = g = h = 0; h < l.length; h++) {
+        q = (q + 1) % 256;
+        g = (g + k[q]) % 256;
+        tmp = k[q];
+        k[q] = k[g];
+        k[g] = tmp;
+        u += chr(ord(l[h]) ^ (k[(k[q] + k[g]) % 256]))
+    }
+    if (f == "DECODE") {
+        if ((u.substr(0, 10) == 0 || u.substr(0, 10) - time() > 0) && u.substr(10, 16) == md5(u.substr(26) + o).substr(0, 16)) {
+            u = u.substr(26)
+        } else {
+            u = ""
+        }
+        u = base64.decode(d)
+    }
+    return u
+};
